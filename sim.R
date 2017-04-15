8fc4d2fa0e1ef3acbded586ad983a961320453f4
ben-domingue <ben.domingue@gmail.com>
2017-04-15 16:35:05 -0700

    different kinds of entropy and new output

--- sim.R
+++ sim.R
@@ -1,11 +1,10 @@
 ##these are aux functions. the main simulation is in sim_run.R
 
 sim<-function(L,k) { #this simulates everything based on a few key parameters
-    h<-function(df,k) { #this computes the basic segregation statistic based on a value for k and:
+    h<-function(df,k,ent) { #this computes the basic segregation statistic based on a value for k and:
                                         #a data frame "df" that contains columns
                                         #g which is the group variable and
                                         #o which is the observed variable
-        ent<-function(p) p*(1-p) #ent<-function(p) ifelse(p!=0 & p!=1,-(p*log2(p)+(1-p)*log2(1-p)),0) #see footnote 6
         p<-function(y,k) sum(y<=k)/length(y) #see p. 12 
                                         #
         unique(df$g)->grps
@@ -38,26 +37,26 @@ sim<-function(L,k) { #this simulates everything based on a few key parameters
         }
         do.call("rbind",tmp)->tmp
         data.frame(o=tmp[,3],g=tmp[,1])->df
-        h(df,k)->out[i]
+        h(df,k,ent=ent)->out[i]
         data.frame(o=tmp[,2],g=tmp[,1])->df
-        h(df,k)->out.true[i]
+        h(df,k,ent=ent)->out.true[i]
 ############################################
     }
     c(obs=mean(out),true=mean(out.true))
 }
 
 
-##graphics!
-graf<-function(hold) {
-    do.call("rbind",hold)->xx
-    par(mgp=c(2,1,0))
-    plot(xx[,1],type="l",ylab="seg",xlab="",xaxt="n",,lwd=2,ylim=c(0,1))
-    strsplit(names(hold)," ")->nms
-    sapply(nms,"[",1)->tmp
-    for (i in 1:length(tmp)) mtext(side=1,at=i,tmp[i],line=0.25,,cex=.7)
-    #mtext(side=1,line=.25,at=-2,"sch sd",adj=0)
-    sapply(nms,"[",2)->tmp
-    for (i in 1:length(tmp)) mtext(side=1,at=i,tmp[i],line=1,cex=.7)
-    #mtext(side=1,line=1,at=-2,"err sd",adj=0)
-    lines(xx[,2],type="l",col="red",lwd=2)
-}
+## ##graphics!
+## graf<-function(hold) {
+##     do.call("rbind",hold)->xx
+##     par(mgp=c(2,1,0))
+##     plot(xx[,1],type="l",ylab="seg",xlab="",xaxt="n",,lwd=2,ylim=c(0,1))
+##     strsplit(names(hold)," ")->nms
+##     sapply(nms,"[",1)->tmp
+##     for (i in 1:length(tmp)) mtext(side=1,at=i,tmp[i],line=0.25,,cex=.7)
+##     #mtext(side=1,line=.25,at=-2,"sch sd",adj=0)
+##     sapply(nms,"[",2)->tmp
+##     for (i in 1:length(tmp)) mtext(side=1,at=i,tmp[i],line=1,cex=.7)
+##     #mtext(side=1,line=1,at=-2,"err sd",adj=0)
+##     lines(xx[,2],type="l",col="red",lwd=2)
+## }
--- sim_run.R
+++ sim_run.R
@@ -1,24 +1,33 @@
+ent1<-function(p) p*(1-p) 
+ent2<-function(p) ifelse(p!=0 & p!=1,-(p*log2(p)+(1-p)*log2(1-p)),0) #see footnote 6
+ent.list<-list(r=ent1,h=ent2)
 
-hold<-list()
 ##now we're going to set the various conditions under which the simulation runs
 err.sd<-c(0,.1,.5,1,1.5,2) #these are the SDs for the individual-level errors
 sch.sd<-c(.5) #this is associated with the variation in school-level means
 
-par(mfrow=c(3,3))
+tab<-list()
 for (k in qnorm(seq(.1,.9,by=.1))) {
     L<-list()
-    for (err in err.sd) for (sch in sch.sd) {
-                            list(sig=sch,err.sd=err,Nperschool=500,n.school=50,niter=25)->L[[paste(sch,err)]] #these are the arguments passed to each iteration of the simulation
+    for (err in err.sd) for (sch in sch.sd) for (ent in names(ent.list)) {
+                                                ent.list[[ent]]->ent.fun
+                                                list(sig=sch,err.sd=err,Nperschool=500,n.school=50,niter=25,ent=ent.fun)->L[[paste(k,sch,err,ent)]] #these are the arguments passed to each iteration of the simulation
                         }
     library(parallel)
     makeCluster(4)->cl #modify this (e.g., change 4 to X) where X is the number of local processors. 
     clusterApply(cl,L,sim,k=k)->hold
     stopCluster(cl)
     names(L)->names(hold)
-    graf(hold)
-    mtext(side=3,line=0,paste("k=",k))
-    #legend("topleft",bty="n",title=paste("k=",k,sep=""!),c("observed","true"),lwd=2,col=c("black","red"),lty=1)
+    do.call("rbind",hold)->tab[[as.character(k)]]
 }
+do.call("rbind",tab)->tab
+strsplit(rownames(tab)," ")->txt
+data.frame(tab)->tab
+NULL->rownames(tab)
+data.frame(do.call("rbind",txt))->tmp
+names(tmp)<-c("k","sch.sd","err.sd","segfun")
+data.frame(tmp,tab)->tab
+write.csv(tab,"")
 
 
 
